3d684492036c5112e00e04e0b5582934d84193ea
Glitch (pickle-few-ringer) <none>
2020-09-01 16:34:19 +0000

    🐏🏵 Checkpoint ./server.js:21184821/4230

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ app.get("/", function(req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/whoami", function(req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.lang,
+    // software: req.os
+  });
 });
 
 // listen for requests :)
